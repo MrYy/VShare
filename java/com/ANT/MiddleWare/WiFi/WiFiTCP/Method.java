e3bfdacab36f6161e26179cfc6b03eeeb2865c2a
MrYy <yy1992@bupt.edu.cn>
2016-05-07 11:32:27 +0800

    fix final

--- java/com/ANT/MiddleWare/WiFi/WiFiTCP/Method.java
+++ java/com/ANT/MiddleWare/WiFi/WiFiTCP/Method.java
@@ -50,7 +50,7 @@ public class Method {
                 if (count < 0) break;
                 byteRead += count;
                 //check the last fragment,
-                //try to wait 10 seconds.
+                //try to wait  seconds.
                 if(count==0) {
                     i++;
                     TimeUnit.SECONDS.sleep(1);
