13791e3c523ea744fa45a6d7f3b2bbed2adf7ffc
MrYy <yy1992@bupt.edu.cn>
2016-07-05 21:59:12 +0800

    添加启动界面

--- java/com/ANT/MiddleWare/PartyPlayerActivity/LoginFragment.java
+++ java/com/ANT/MiddleWare/PartyPlayerActivity/LoginFragment.java
@@ -1,11 +1,17 @@
 package com.ANT.MiddleWare.PartyPlayerActivity;
 
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 
 /**
@@ -13,6 +19,12 @@ import android.view.ViewGroup;
  */
 public class LoginFragment extends Fragment {
 
+
+    private View view;
+    private Activity context;
+    private CheckBox checkBoxRem;
+    private CheckBox checkBoxPublis;
+    private EditText editText;
 
     public LoginFragment() {
         // Required empty public constructor
@@ -23,7 +35,26 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        view =  inflater.inflate(R.layout.fragment_login, container, false);
+        context = getActivity();
+        checkBoxRem = (CheckBox)context.findViewById(R.id.checkbox_remember_account);
+        checkBoxPublis = (CheckBox) context.findViewById(R.id.checkbox_publish_video);
+        editText = (EditText) context.findViewById(R.id.edittext_name);
+        ((Button)context.findViewById(R.id.button_login)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = editText.getText().toString().trim();
+                if (editText.equals("")) {
+                    name = "小蚂蚁";
+                }
+                Intent intent = new Intent(getActivity(), ViewVideoActivity.class);
+                intent.putExtra(context.getString(R.string.user_name), name);
+                intent.putExtra(context.getString(R.string.publish_video), checkBoxPublis.isChecked());
+                intent.putExtra("保存用户昵称", checkBoxRem.isChecked());
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 
 }
---
+++ java/com/ANT/MiddleWare/PartyPlayerActivity/ViewVideoActivity.java
@@ -0,0 +1,12 @@
+package com.ANT.MiddleWare.PartyPlayerActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ViewVideoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
