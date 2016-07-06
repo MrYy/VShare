7a8be674bd048fe0767f2b1fb8c3f08e51f5237a
MrYy <yy1992@bupt.edu.cn>
2016-07-06 11:42:08 +0800

    添加fragment

--- java/com/ANT/MiddleWare/PartyPlayerActivity/ViewVideoActivity.java
+++ java/com/ANT/MiddleWare/PartyPlayerActivity/ViewVideoActivity.java
@@ -1,30 +1,67 @@
 package com.ANT.MiddleWare.PartyPlayerActivity;
 
-import android.app.Activity;
+import android.content.Context;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
 
 import com.ANT.MiddleWare.DASHProxyServer.DashProxyServer;
+import com.ANT.MiddleWare.WiFi.WiFiTCP.Method;
 
-import java.io.IOException;
-
-public class ViewVideoActivity extends Activity {
+public class ViewVideoActivity extends FragmentActivity implements View.OnClickListener {
 
     private Button buttonView;
     private EditText editTextLocation;
     private DashProxyServer server = new DashProxyServer();
     public static ConfigureData configureData = new ConfigureData(null);
+    private Switch switchButton;
+    private WifiManager wifiManager;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_view_video);
-        buttonView = (Button) findViewById(R.id.button_view_video);
-        editTextLocation = (EditText) findViewById(R.id.edittext_video_location);
-        initDashProxy();
+        init();
     }
 
+    private void init() {
+        buttonView = (Button) findViewById(R.id.button_view_video);
+        editTextLocation = (EditText) findViewById(R.id.edittext_video_location);
+        switchButton = (Switch) findViewById(R.id.switch_publish_video);
+        switchButton.setOnClickListener(this);
+        switchButton.setChecked(false);
+        wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+        Fragment video = new VideoFragment();
+        Fragment chatroom = new ChatFragment();
+        ft.add(R.id.fragment_chat_room,chatroom);
+        ft.add(R.id.fragment_video_player, video);
+        ft.commit();
+        initDashProxy();
+    }
     private void initDashProxy() {
             configureData.setWorkingMode(ConfigureData.WorkMode.LOCAL_MODE);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.switch_publish_video:
+                if (switchButton.isChecked()) {
+                    Method.changeApState(this,wifiManager,true);
+                }else {
+                    Method.changeApState(this,wifiManager,false);
+                }
+                break;
+        }
     }
 }
