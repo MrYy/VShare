180df549e8228a54794617114a3cbb3eb593b6ea
MrYy <yy1992@bupt.edu.cn>
2016-07-11 10:50:54 +0800

    register dialog, and format file directory

--- java/com/ANT/MiddleWare/PartyPlayerActivity/util/LoginDialog.java
+++ java/com/ANT/MiddleWare/PartyPlayerActivity/util/LoginDialog.java
@@ -4,6 +4,12 @@ import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.view.animation.ScaleAnimation;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.ANT.MiddleWare.PartyPlayerActivity.R;
 
@@ -11,6 +17,11 @@ import com.ANT.MiddleWare.PartyPlayerActivity.R;
  * Created by zxc on 2016/7/11.
  */
 public class LoginDialog extends Dialog implements View.OnClickListener{
+    private View mDialogView;
+    private Button registerButton;
+    private EditText name;
+    private EditText password;
+
     public LoginDialog(Context context) {
         this(context,R.style.alert_dialog);
     }
@@ -20,16 +31,27 @@ public class LoginDialog extends Dialog implements View.OnClickListener{
         setCancelable(true);
         setCanceledOnTouchOutside(true);
 
+
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_dialog);
+        setContentView(R.layout.register);
+        mDialogView = getWindow().getDecorView().findViewById(android.R.id.content);
+        mDialogView.startAnimation(AnimationUtils.loadAnimation(getContext(),R.anim.modal_in));
+        registerButton = (Button) findViewById(R.id.button_register);
+        name = (EditText) findViewById(R.id.edittext_name);
+        password = (EditText) findViewById(R.id.edittext_password);
+        registerButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_register:
 
+                break;
+        }
     }
 }
