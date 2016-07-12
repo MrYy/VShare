e183acda3e99b5fc7d3c3cfc8327a177cf5b82e6
MrYy <yy1992@bupt.edu.cn>
2016-07-12 15:18:00 +0800

    fix

--- java/com/ANT/MiddleWare/PartyPlayerActivity/UserActivity.java
+++ java/com/ANT/MiddleWare/PartyPlayerActivity/UserActivity.java
@@ -2,16 +2,17 @@ package com.ANT.MiddleWare.PartyPlayerActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 
 import com.ANT.MiddleWare.PartyPlayerActivity.bean.MenuLayout;
 
-public class UserActivity extends Activity {
+public class UserActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.user_activity);
-
+        setContentView(R.layout.fragment_information);
     }
 
     @Override
