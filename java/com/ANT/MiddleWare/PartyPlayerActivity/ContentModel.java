990c02b283acef1001dd9fb0ef980195050d054e
fyf <1252594532@qq.com>
2016-07-14 22:03:10 +0800

    fixaaaaaa

---
+++ java/com/ANT/MiddleWare/PartyPlayerActivity/ContentModel.java
@@ -0,0 +1,28 @@
+package com.ANT.MiddleWare.PartyPlayerActivity;
+
+public class ContentModel {
+    private int imageView;
+    private String text;
+
+    public ContentModel(int imageView, String text) {
+        super();
+        this.imageView = imageView;
+        this.text = text;
+    }
+
+    public int getImageView() {
+        return imageView;
+    }
+
+    public void setImageView(int imageView) {
+        this.imageView = imageView;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
