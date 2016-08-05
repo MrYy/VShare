fe724de553862f63c90510560d08dad3020bc461
MrYy <yy1992@bupt.edu.cn>
2016-08-05 18:05:07 +0800

    改善连接问题

--- java/com/ANT/MiddleWare/WiFi/WiFiNCP2/Client.java
+++ java/com/ANT/MiddleWare/WiFi/WiFiNCP2/Client.java
@@ -6,17 +6,22 @@ import com.ANT.MiddleWare.Entities.FileFragment;
 import com.ANT.MiddleWare.PartyPlayerActivity.ViewVideoActivity;
 import com.ANT.MiddleWare.PartyPlayerActivity.bean.Message;
 import com.ANT.MiddleWare.PartyPlayerActivity.bean.SendTask;
+import com.ANT.MiddleWare.PartyPlayerActivity.policy.directStatus.IsOwner;
 import com.ANT.MiddleWare.PartyPlayerActivity.util.Method;
 import com.ANT.MiddleWare.WiFi.WiFiTCP.MyException;
 
 import java.io.IOException;
+import java.net.ConnectException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -48,7 +53,7 @@ public class Client implements Runnable {
             sc.configureBlocking(false);
             sc.socket().getTcpNoDelay();
             Selector selector = Selector.open();
-            sc.register(selector, SelectionKey.OP_READ|SelectionKey.OP_WRITE);
+            sc.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
             while (true) {
                 int selected = selector.select();
                 if (selected == 0) continue;
@@ -56,15 +61,15 @@ public class Client implements Runnable {
                 Iterator ite = mKeys.iterator();
                 while (ite.hasNext()) {
                     mKey = (SelectionKey) ite.next();
-                        if (mKey.isReadable()) {
+                    if (mKey.isReadable()) {
                         //在这里准备添加报头
 //                            Log.d(TAG, "client is reading");
-                            SocketChannel mSc = (SocketChannel) mKey.channel();
+                        SocketChannel mSc = (SocketChannel) mKey.channel();
                         Method.read(mSc);
 //                            Log.d(TAG, "finish reading");
                     } else if (mKey.isWritable()) {
 //                            Log.d(TAG, "client is writing");
-                        SocketChannel mSc = (SocketChannel)mKey.channel();
+                        SocketChannel mSc = (SocketChannel) mKey.channel();
 //                            TimeUnit.SECONDS.sleep(3);
 //                        Message testMsg = new Message();
 //                            testMsg.setMessage(Method.getRandomString(300));
@@ -74,7 +79,7 @@ public class Client implements Runnable {
                                 SendTask sendTask = ViewVideoActivity.sendMessageQueue.poll();
                                 Message msg = sendTask.getMsg();
                                 msg.setName(ViewVideoActivity.userName);
-                                Method.send(msg,mSc);
+                                Method.send(msg, mSc);
                             }
                             while (!ViewVideoActivity.taskMessageQueue.isEmpty()) {
                                 //发送报文
@@ -90,6 +95,13 @@ public class Client implements Runnable {
                     }
                     ite.remove();
                 }
+            }
+        } catch (ConnectException ce) {
+            ExecutorService es = Executors.newFixedThreadPool(1);
+            try {
+                es.execute(new Client(InetAddress.getByName(IsOwner.ip), 12345));
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
             }
         } catch (IOException e) {
             e.printStackTrace();
